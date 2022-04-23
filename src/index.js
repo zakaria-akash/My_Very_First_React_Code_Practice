c9e7e51843c2045a5116fd27bbbbdc14c028ce20
Zakaria Ibrahim <59124067+zakaria-akash@users.noreply.github.com>
2022-04-24 01:17:48 +0600

    adding second paragrapgh tag to index.js file.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ ReactDOM.render(
     <center>
       <h1>Hello World!</h1>
       <p>I am Zakaria Ibrahim.</p>
+      <p>This is my first ever REACT coding practice!</p>
     </center>
   </div>,
   document.getElementById("root")
