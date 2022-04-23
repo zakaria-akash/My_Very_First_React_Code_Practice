bb29c9b421e3ab558ec1c8eba64da2264264aa27
Zakaria Ibrahim <59124067+zakaria-akash@users.noreply.github.com>
2022-04-24 00:58:56 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <center>
+      <h1>Hello World!</h1>
+      <p>I am Zakaria Ibrahim.</p>
+    </center>
+  </div>,
+  document.getElementById("root")
+);
